c122242637a98fcfc92c5528fb4027d6acc01156
arifik0502 <137647192+arifik0502@users.noreply.github.com>
2026-09-13 20:42:05 +0800

    Remove Google login button from login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -166,15 +166,6 @@
             <div class="relative flex justify-center text-xs uppercase"><span class="bg-white px-2 text-gray-400">or</span></div>
         </div>
 
-        <a href="{{ route('auth.google.redirect') }}" class="btn-secondary w-full">
-            <svg class="w-4 h-4" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-                <path fill="#FFC107" d="M43.6 20.5H42V20H24v8h11.3c-1.6 4.6-6 8-11.3 8-6.6 0-12-5.4-12-12s5.4-12 12-12c3.1 0 5.8 1.1 8 3l6-6C34.5 5.1 29.5 3 24 3 12.4 3 3 12.4 3 24s9.4 21 21 21 21-9.4 21-21c0-1.4-.1-2.7-.4-3.5z"/>
-                <path fill="#FF3D00" d="M6.3 14.7l6.6 4.8C14.6 15.9 18.9 13 24 13c3.1 0 5.8 1.1 8 3l6-6C34.5 5.1 29.5 3 24 3 16.3 3 9.7 7.3 6.3 14.7z"/>
-                <path fill="#4CAF50" d="M24 45c5.4 0 10.3-2.1 14-5.5l-6.5-5.3C29.5 35.9 26.9 37 24 37c-5.3 0-9.7-3.4-11.3-8.1l-6.5 5C9.6 40.6 16.2 45 24 45z"/>
-                <path fill="#1976D2" d="M43.6 20.5H42V20H24v8h11.3c-.8 2.3-2.2 4.2-4.1 5.6l6.5 5.3C41 35.5 45 30.3 45 24c0-1.4-.1-2.7-.4-3.5z"/>
-            </svg>
-            Continue with Google
-        </a>
 
         <p class="text-sm text-center text-gray-600">
             Don't have an account?
